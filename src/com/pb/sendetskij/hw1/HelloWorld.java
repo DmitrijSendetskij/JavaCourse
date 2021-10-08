f16c8428117c9e1e16de68e68ff7448428c0708b
DmitrijSendetskij <dmitrij.sendetskij@gmail.com>
2021-10-08 23:53:54 +0300

    Commit 1.0.2

--- src/com/pb/sendetskij/hw1/HelloWorld.java
+++ src/com/pb/sendetskij/hw1/HelloWorld.java
@@ -1,12 +1,12 @@
 package com.pb.sendetskij.hw1;
 /*
 1. Скачать и установить JDK на https://oracle.com/
-2. Скачать и установить git https://git-scm.com/download/
+2. Скачать и установить Git https://git-scm.com/download/
 3. Пройти регистрацию на https://github.com
 4. https://github.com создать токен Setting / Developer setting / Personal access token / Generate new token
-5. Скачать и устновить IntelliJ IDEA https://www.jetbrains.com/ru-ru/idea/download/#section=windows
+5. Скачать и устновить IntelliJ IDEA https://www.jetbrains.com/ru-ru/idea/download
 6. Создать проект в IntelliJ IDEA
-7. В IntelliJ IDEA File -  Setting / Version Control / GIT / Указать путь к фалй git.exe нажать Test для проверки версии гита
+7. В IntelliJ IDEA - File / Setting / Version Control / GIT / Указать путь к файлу git.exe нажать Test для проверки версии гита
 8. В IntelliJ IDEA добавить токен п4. - Setting / Version Control / GITHub / + / Use token
 9. В IntelliJ IDEA - VCS / для проекта добавить систему версий GIT
 10. В IntelliJ IDEA  создать проект на Git - VCS / Import Into Version Control / Share Project on GitHub
